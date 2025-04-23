a28121cf99ec0d507b32f2209569f61a5ad8d677
mrdanwa <wdanrui@gmail.com>
2025-04-24 00:51:54 +0200

    commit

--- assets/js/core.js
+++ assets/js/core.js
@@ -472,40 +472,6 @@ if (document.createElement("svg").getAttributeNS) {
   }
 }
 
-////////////////////////////////////////
-//
-// Universal contact form ajax submission
-//
-////////////////////////////////////////
-
-$(document).ready(function () {
-  if ($(".zform").length) {
-    $(".zform").each(function () {
-      var $form = $(this);
-
-      $form.on("submit", function (e) {
-        e.preventDefault();
-
-        var $submit = $form.find(":submit"),
-          submitText = $submit.val();
-
-        $submit.val("Sending...");
-
-        $.ajax({
-          type: "post",
-          url: "assets/php/form-processor.php",
-          data: $(this).serialize(), // again, keep generic so this applies to any form
-          success: function (result) {
-            $form.find(".zform-feedback").html(result);
-            $submit.val(submitText);
-            $form.trigger("reset");
-          },
-        });
-      });
-    });
-  }
-});
-
 //////////////////////////////////
 //
 // Sementic UI
