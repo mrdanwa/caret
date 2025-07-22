232e3894263354ea463899a2a4ef0ae69348807e
Danrui Wang <danruiwang@Danruis-MacBook-Pro.local>
2025-07-22 16:27:57 +0200

    commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,96 +2,100 @@
 document.addEventListener("DOMContentLoaded", function () {
   const newsletterForm = document.querySelector(".newsletter-form");
 
-  newsletterForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  if (newsletterForm) {
+    newsletterForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    const inputGroup = this.querySelector(".input-group");
-    const emailInput = this.querySelector('input[type="email"]');
-    const email = emailInput.value.trim();
+      const inputGroup = this.querySelector(".input-group");
+      const emailInput = this.querySelector('input[type="email"]');
+      const email = emailInput.value.trim();
 
-    // Save original content to restore later
-    const originalContent = inputGroup.innerHTML;
+      // Save original content to restore later
+      const originalContent = inputGroup.innerHTML;
 
-    // Send the request to the API
-    fetch("https://caret-ek3gf.ondigitalocean.app/api/newsletter/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({ email: email }),
-    }).then(() => {
-      inputGroup.innerHTML =
-        '<div style="width: 100%; padding: 0.9rem 1.2rem; color: white; font-size: 16px; font-weight: bold; text-align: center;">¡Gracias por suscribirte!</div>';
+      // Send the request to the API
+      fetch("https://caret-ek3gf.ondigitalocean.app/api/newsletter/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ email: email }),
+      }).then(() => {
+        inputGroup.innerHTML =
+          '<div style="width: 100%; padding: 0.9rem 1.2rem; color: white; font-size: 16px; font-weight: bold; text-align: center;">¡Gracias por suscribirte!</div>';
 
-      setTimeout(() => {
-        inputGroup.innerHTML = originalContent;
-        inputGroup.querySelector('input[type="email"]').value = "";
-      }, 3000);
+        setTimeout(() => {
+          inputGroup.innerHTML = originalContent;
+          inputGroup.querySelector('input[type="email"]').value = "";
+        }, 3000);
+      });
     });
-  });
+  }
 });
 
 // Contact form integration
 document.addEventListener("DOMContentLoaded", function () {
   const contactForm = document.getElementById("contact-form");
-  const feedbackDiv = contactForm.querySelector(".zform-feedback");
+  if (contactForm) {
+    const feedbackDiv = contactForm.querySelector(".zform-feedback");
 
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+    contactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    const name = document.getElementById("contact-name").value.trim();
-    const phone = document.getElementById("contact-phone").value.trim();
-    const email = document.getElementById("contact-email").value.trim();
-    const message = document.getElementById("contact-message").value.trim();
+      const name = document.getElementById("contact-name").value.trim();
+      const phone = document.getElementById("contact-phone").value.trim();
+      const email = document.getElementById("contact-email").value.trim();
+      const message = document.getElementById("contact-message").value.trim();
 
-    // Save original content to restore later
-    const originalContent = contactForm.innerHTML;
+      // Save original content to restore later
+      const originalContent = contactForm.innerHTML;
 
-    // Send the request to the API
-    fetch("https://caret-ek3gf.ondigitalocean.app/api/contact/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        phone: phone,
-        message: message,
-      }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
+      // Send the request to the API
+      fetch("https://caret-ek3gf.ondigitalocean.app/api/contact/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          phone: phone,
+          message: message,
+        }),
       })
-      .then(() => {
-        // Show success message
-        feedbackDiv.innerHTML =
-          '<div style="color: #004225; font-family: "Montserrat", sans-serif;">¡Gracias! Hemos recibido tu mensaje.</div>';
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(() => {
+          // Show success message
+          feedbackDiv.innerHTML =
+            '<div style="color: #004225; font-family: "Montserrat", sans-serif;">¡Gracias! Hemos recibido tu mensaje.</div>';
 
-        // Reset form
-        contactForm.reset();
+          // Reset form
+          contactForm.reset();
 
-        // Restore original content after 3 seconds
-        setTimeout(() => {
-          feedbackDiv.innerHTML = "";
-        }, 3000);
-      })
-      .catch((error) => {
-        // Show error message
-        feedbackDiv.innerHTML =
-          '<div style="color: #721c24; font-family: "Montserrat", sans-serif;">Ups... algo salió mal. Intenta de nuevo.</div>';
+          // Restore original content after 3 seconds
+          setTimeout(() => {
+            feedbackDiv.innerHTML = "";
+          }, 3000);
+        })
+        .catch((error) => {
+          // Show error message
+          feedbackDiv.innerHTML =
+            '<div style="color: #721c24; font-family: "Montserrat", sans-serif;">Ups... algo salió mal. Intenta de nuevo.</div>';
 
-        // Clear error message after 3 seconds
-        setTimeout(() => {
-          feedbackDiv.innerHTML = "";
-        }, 3000);
-      });
-  });
+          // Clear error message after 3 seconds
+          setTimeout(() => {
+            feedbackDiv.innerHTML = "";
+          }, 3000);
+        });
+    });
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
